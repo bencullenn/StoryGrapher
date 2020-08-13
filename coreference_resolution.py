7413124d2939f3cdba3ca2889073c9251b36cb9b
bencullenn <bencullenn@gmail.com>
2020-08-13 17:12:28 -0600

    Updated coference_resolution script to support chunking allowing it to process text files of any size

--- coreference_resolution.py
+++ coreference_resolution.py
@@ -16,6 +16,8 @@ from sys import argv
 from os import listdir
 from os.path import isfile, join
 import allennlp_models.coref
+import spacy
+import math
 
 IGNORE = {
     "a", "an", "the", "that", "this", "another",
@@ -61,6 +63,50 @@ def get_name(cluster, document):
     used_refs.add(name + str(k))
     return name + str(k)
 
+def chunkText(text):
+    spacy_sent = spacy.load('en_core_web_sm')
+    spacy_sent.add_pipe(spacy_sent.create_pipe('sentencizer'))
+    print("Generated Spacy")
+
+    doc = spacy_sent(text)
+    sentences = []
+    chunks = []
+    sent_num = 0
+    max_sent_per_chunk = 250
+    
+    for sent in doc.sents:
+        sentences.append(sent)
+        sent_num += 1
+    
+    chunk_num = math.ceil(sent_num/max_sent_per_chunk)
+    chunk_size = math.ceil(sent_num/chunk_num)
+    print(sent_num, " sentences were extracted. Splitting into ", chunk_num, " chunks of ", chunk_size, " sentences each.") 
+    
+    slice_index = 0
+    for i in range(chunk_num):
+        chunk = []
+        
+        if slice_index + chunk_size < len(sentences):
+            chunk = sentences[slice_index:slice_index + chunk_size]
+            slice_index += chunk_size
+        else:
+            chunk = sentences[slice_index:-1]
+
+        text = ""
+        for sent in chunk:
+            text += sent.text
+        
+        chunks.append(text)
+
+    print("Quality Check")
+    
+    chunk_index = 1
+    for chunk in chunks:
+        print("Chunk #", chunk_index, " has an length of ", len(chunk))
+
+    return chunks   
+
+
 #%% Initialization
 print("Cuda enabled:", torch.cuda.current_device() == 0)
 print("Loading models...")
@@ -72,19 +118,25 @@ print("PATHS DEBUG:", paths)
 for p in paths:
     with open(path_in + p) as f:
         text = f.read()
+    
+    chunks = chunkText(text)
+    total_result = ""
+    results = []
+    for chunk in chunks:
+        result = get_coref_prediction(coref_predictor, chunk)
+        clusters, result = result['clusters'], result['document'].copy()
+        refs = [get_name(c, result) for c in clusters]
+        # print(f"Coref Clusters: {clusters}")
 
-    result = get_coref_prediction(coref_predictor, text)
-    clusters, result = result['clusters'], result['document'].copy()
-    refs = [get_name(c, result) for c in clusters]
-    # print(f"Coref Clusters: {clusters}")
-
-    for j, c in enumerate(clusters):
-        for span in c:
-            for m in range(span[0], span[1]+1):
-                result[m] = None
-            result[span[0]] = refs[j]
-    result = filter(lambda s: s is not None, result)
-    result = ' '.join(result)
-
+        for j, c in enumerate(clusters):
+            for span in c:
+                for m in range(span[0], span[1]+1):
+                    result[m] = None
+                result[span[0]] = refs[j]
+        result = filter(lambda s: s is not None, result)
+        result = ' '.join(result)
+        results.append(result)
+        
+    total_result = total_result.join(results)
     with open(path_out + p, "w+") as f:
-        f.write(result)
+        f.write(total_result)   
